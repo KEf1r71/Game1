17b4905f1abfd2f34753e90081466eefbf98f41f
KEf1r71 <leonid.yarosh7709@gmail.com>
2026-05-04 16:36:37 +0300

    патч5 кейсы

--- Game.py
+++ Game.py
@@ -2,18 +2,22 @@ import random
 import time
 from enum import Enum
 
+
 class Rarity(Enum):
-    common="Обычный"
-    rare="Редкий"
-    expensive="Ценный"
+    common = "Обычный"
+    rare = "Редкий"
+    expensive = "Ценный"
+
+
 class Item:
-    def __init__(self, name, rarity, stat,itemType):
+    def __init__(self, name, rarity, stat, itemType):
         self.name = name
         self.rarity = rarity
         self.stat = stat
         self.itemType = itemType
+
     @staticmethod
-    def ChooseItem(createDrop,Rarity,Type):
+    def ChooseItem(createDrop, Rarity, Type):
         return random.choice(createDrop[Type][Rarity])
         return random.choice(createDrop[Type][Rarity])
 
@@ -41,44 +45,35 @@ class Character:
         self.cMaxHp = hp
         self.cash = 0
         self.medsSlot = False
-        self.inventory=[]
-
+        self.inventory = []
 
     def inventoryView(self):
         if not self.inventory:
             print("Рюкзак пуст. ")
             return True
         else:
-            for i, item in enumerate(self.inventory,0):
-                print(f"{i} {item.name,item.rarity}")
+            for i, item in enumerate(self.inventory, 0):
+                print(f"{i} {item.name}")
             return True
         return False
 
-
-
-    def inventory(self,i):
+    def inventory(self, i):
         self.inventoryView()
-        invItem=self.inventory[i]
+        invItem = self.inventory[i]
         if invItem.itemType == "meds":
-            if self.hp+invItem.stat > self.cMaxHp:
+            if self.hp + invItem.stat > self.cMaxHp:
                 self.hp = self.cMaxHp
                 self.inventory.remove(invItem)
                 print(f"Вы восстановили здоровье до максимума ({self.cMaxHp})")
             elif self.hp == self.cMaxHp:
-                #медикамент не тратится
+                # медикамент не тратится
                 print("У вас максимальный запас здоровья, вы не можете использовать медикамент.")
             else:
                 self.hp += invItem.stat
                 self.inventory.remove(invItem)
                 print(f"Вы восстановили здоровье до {self.hp}/{self.cMaxHp}")
 
-
-        #добавить ограничения по типу невозможности хилиться больше максимального количества хп
-
-
-
-
-
+        # добавить ограничения по типу невозможности хилиться больше максимального количества хп
 
     # создать дефолтного дебильчика с базовыми статами и выдать ему какое нибудь изначальное оружие
     def talentUse(self):
@@ -158,6 +153,42 @@ class Character:
         print(f"У тебя осталось {self.talent} очков талантов, готовых к распределению. ")
         print(f"До следующего уровня осталось {self.remainingXp - self.currentXp}")
 
+    def take_meds(self, item):
+        self.medsSlot = item
+        self.inventory.append(item)
+        print(f"{item.name} был добавлен в инвентарь")
+        return True
+
+    def drop_meds(self, item):
+        self.medsSlot = False
+        self.inventory.remove(item)
+        print(f"{item.name} был удален из инвентаря")
+
+    def take_weapon(self, item):
+        self.slot = True
+        self.damage += item.physDmg + item.magicDmg
+        self.speed += item.fireRate
+        self.fRange += item.range
+        self.gAccuracy += item.accuracy
+        self.cAttackPoints = item.fireRate // 5
+
+    def drop_weapon(self, item):
+        self.slot = False
+        self.damage -= item.physDmg + item.magicDmg
+        self.speed -= item.fireRate
+        self.fRange -= item.range
+        self.gAccuracy -= item.accuracy
+        self.cAttackPoints = 1
+
+    def take_armor(self, item):
+        self.armorSlot = True
+        self.cPhysDef += item.pDefence
+        self.cMgcDef += item.mDefence
+
+    def drop_armor(self, item):
+        self.armorSlot = False
+        self.cPhysDef -= item.pDefence
+        self.cMgcDef -= item.mDefence
 
 name, hp, gun, speed, damage = None, None, None, None, None
 
@@ -168,15 +199,7 @@ class Armor:
         self.pDefence = pDefence
         self.mDefence = mDefence
 
-    def take(self, character):
-        character.armorSlot = True
-        character.PhysDefence += self.pDefence
-        character.cMgcDefence += self.mDefence
 
-    def drop(self, character):
-        character.armorSlot = False
-        character.PhysDefence -= self.pDefence
-        character.cMgcDefence -= self.mDefence
 
 
 class Weapon:
@@ -188,21 +211,7 @@ class Weapon:
         self.range = range
         self.accuracy = accuracy
 
-    def take(self, character):
-        character.slot = True
-        character.damage += self.physDmg + self.magicDmg
-        character.speed += self.fireRate
-        character.fRange += self.range
-        character.gAccuracy += self.accuracy
-        character.cAttackPoints = self.fireRate // 5
 
-    def drop(self, character):
-        character.slot = False
-        character.damage -= self.physDmg + self.magicDmg
-        character.speed -= self.fireRate
-        character.fRange -= self.range
-        character.gAccuracy -= self.accuracy
-        character.cAttackPoints = 1
 
 
 class Meds:
@@ -211,19 +220,9 @@ class Meds:
         self.healHp = healHp
         self.actionPointsUse = actionPointsUse
 
-    def take(self, character, item):
-        character.medsSlot = item
-        character.inventory.append(item)
-        return f"{item.name} был добавлен в инвентарь"
-
-    def drop(self, character, item):
-        character.medsSlot = False
-        character.inventory.remove(item)
-        return f"{item.name} был удален из инвентаря"
-
 
 class Enemy:
-    def __init__(self, name, hp, gun, physDmg, mgcDmg, fRange,cArmor, gAccuracy, cPhysDef, cMgcDef, giveXp, lvl,):
+    def __init__(self, name, hp, gun, physDmg, mgcDmg, fRange, cArmor, gAccuracy, cPhysDef, cMgcDef, giveXp, lvl, ):
         self.name = name
         self.hp = hp
         self.gun = gun
@@ -237,6 +236,7 @@ class Enemy:
         self.giveXp = giveXp
         self.lvl = lvl
         self.maxHp = hp
+
     @staticmethod
     def ChoiceEnemy(cLvl):
         if cLvl <= 50:
@@ -252,8 +252,6 @@ class Enemy:
             if abs(i.lvl - eLvl) <= 5:
                 avaliableEnemy.append(i)
         return random.choice(avaliableEnemy)
-
-
 
 
 # добавить возможность подбежать к врагу
@@ -301,15 +299,15 @@ def CrateItems(caseType, crateRarity):
             return random.choice(CrateDrop["armor"][3])
 
 
-def CaseOpen(character, caseRarity):
+def CaseOpen(character):
     global armory, armor, meds
     print("Вы роетесь в сундуке", end="")
     for i in range(6):
         time.sleep(0.3)
         print(".", end="")
     print()
-    caseType = random.choice(["meds", "Weapon", "Armor"])
-    if character.lvl <= 10:
+    caseType = random.choice(["meds", "weapon", "armor"])
+    if character.cLvl <= 10:
         caseChance = random.random()
         if caseChance <= 0.9:
             crateRarity = 1
@@ -317,7 +315,7 @@ def CaseOpen(character, caseRarity):
             crateRarity = 2
         else:
             crateRarity = 3
-    if character.lvl <= 20:
+    if character.cLvl <= 20:
         caseChance = random.random()
         if caseChance <= 0.7:
             crateRarity = 1
@@ -325,7 +323,7 @@ def CaseOpen(character, caseRarity):
             crateRarity = 2
         else:
             crateRarity = 3
-    if character.lvl <= 40:
+    if character.cLvl <= 40:
         caseChance = random.random()
         if caseChance <= 0.5:
             crateRarity = 1
@@ -333,7 +331,7 @@ def CaseOpen(character, caseRarity):
             crateRarity = 2
         else:
             crateRarity = 3
-    if character.lvl <= 60:
+    if character.cLvl <= 60:
         caseChance = random.random()
         if caseChance <= 0.2:
             crateRarity = 1
@@ -343,36 +341,36 @@ def CaseOpen(character, caseRarity):
             crateRarity = 3
     crateName = case_types[caseType][crateRarity]
     givenDrop = CrateItems(caseType, crateRarity)
-    print(f"Вы нашли {givenDrop} в {crateName}")
+    print(f"Вы нашли {givenDrop.name} в {crateName}")
     print("1 чтобы экипировать")
     print("2 чтобы оставить")
-    if caseType == "Weapon":
+    if caseType == "weapon":
         try:
             dropChoice = int(input())
         except ValueError as e:
             print(e)
         if dropChoice == 1:
-            Weapon.take(character)
+            character.take_weapon(givenDrop)
         else:
-            Weapon.drop(character)
-    if caseType == "Meds":
+            print("Решив, что данная единица вооружения говно, вы прошли мимо.")
+    if caseType == "meds":
         try:
             dropChoice = int(input())
         except ValueError as e:
             print(e)
         if dropChoice == 1:
-            Meds.take(character)
+            character.take_meds(givenDrop)
         else:
-            Meds.drop(character)
-    if caseType == "Armor":
+            print("Решив что данный медикамент вам не нужен, или непригоден для использования вы его не взяли.")
+    if caseType == "armor":
         try:
             dropChoice = int(input())
         except ValueError as e:
             print(e)
         if dropChoice == 1:
-            Armor.take(character)
+            print("Решив что вы слишком горды для 3го класса брони, эта железка осталась на полу.")
         else:
-            Armor.drop(character)
+            character.drop_armor(givenDrop)
 
 
 # список медикаментов, добвить оружия и добавить стату очков атаки или оптимизировать скорострельность под очки атаки.
@@ -391,9 +389,7 @@ def LutiyFight(character, currentEnemy):
         attackPoints = character.cAttackPoints
         actionPoints = character.cActionPoints
 
-
-
-        while (attackPoints > 0 or actionPoints > 0) and currentEnemy.hp>=0:
+        while (attackPoints > 0 or actionPoints > 0) and currentEnemy.hp >= 0:
             if character.cActionPoints <= 0 and character.cAttackPoints <= 0:
                 print("Вы потратили все очки действий и аттаки, ход передается врагу.")
                 break
@@ -465,7 +461,6 @@ def LutiyFight(character, currentEnemy):
             if battleChoice == 6:
                 break
 
-
         if currentEnemy.hp > 0:
             enemyHitChance = currentEnemy.gAccuracy - max(0, (distance - currentEnemy.fRange) // 10)
             if distance >= currentEnemy.fRange:
@@ -505,27 +500,27 @@ def LutiyFight(character, currentEnemy):
 
 
 # Продумать алгоритм действий внутри битвы, для побега и динамической прокачки.
-enemy = [Enemy("Goblin", 15, "Wooden Stick", 10, 5, 30, "Burlap Clothes", 5, 5, 2, 1,1),
-         Enemy("Goblin With A Magic Stick", 20, "Magic(?) Stick", 15, 25, 20, "Burlap Clothes", 5, 5, 3, 3,1),
+enemy = [Enemy("Goblin", 15, "Wooden Stick", 10, 5, 30, "Burlap Clothes", 5, 5, 2, 1, 1),
+         Enemy("Goblin With A Magic Stick", 20, "Magic(?) Stick", 15, 25, 20, "Burlap Clothes", 5, 5, 3, 3, 1),
          Enemy("Goblin, Who Thinks He's A Warrior", 30, "Wooden Sword (Kind Of)", 20, 10, 30, "Poor Wooden Shield", 10,
-               5, 5, 5,3),
-         Enemy("Skeleton", 25, "Stone Sword", 20, 13, 30, "Nothing...", 10, 0, 7, 10,3),
-         Enemy("Skeleton Wizard", 25, "Magic Stick", 23, 25, 40, "Old Magic Cape", 10, 15, 10, 10,5),
-         Enemy("A Guy With A Knife", 30, "Bayonet", 30, 5, 60, "Nice Set Of Clothes", 20, 10, 10, 15,5),
+               5, 5, 5, 3),
+         Enemy("Skeleton", 25, "Stone Sword", 20, 13, 30, "Nothing...", 10, 0, 7, 10, 3),
+         Enemy("Skeleton Wizard", 25, "Magic Stick", 23, 25, 40, "Old Magic Cape", 10, 15, 10, 10, 5),
+         Enemy("A Guy With A Knife", 30, "Bayonet", 30, 5, 60, "Nice Set Of Clothes", 20, 10, 10, 15, 5),
          Enemy("Beginner Wizard", 30, "A Spell Book (Over 100 Spells inside!)", 20, 25, 40,
-               "Enchanted Clothes (Whole set for only 9.99!)", 5, 15, 13, 15,7),
-         Enemy("Armed Bum", 30, "Handmade Sword", 40, 5, 20, "Coat", 10, 10, 20, 20,8),
-         Enemy("FireArmed Bum", 30, "Poor Handmade Hammer Pistol", 30, 13, 25, "Coat", 10, 10, 22, 20,10),
-         Enemy("Wolf", 50, "Teeth, I guess?", 40, 3, 50, "Nothing...", 5, 5, 30, 25,10),
+               "Enchanted Clothes (Whole set for only 9.99!)", 5, 15, 13, 15, 7),
+         Enemy("Armed Bum", 30, "Handmade Sword", 40, 5, 20, "Coat", 10, 10, 20, 20, 8),
+         Enemy("FireArmed Bum", 30, "Poor Handmade Hammer Pistol", 30, 13, 25, "Coat", 10, 10, 22, 20, 10),
+         Enemy("Wolf", 50, "Teeth, I guess?", 40, 3, 50, "Nothing...", 5, 5, 30, 25, 10),
          Enemy("An Alright Wizard", 30, "Magic Stick. A really simple one", 30, 35, 40, "Simple Enchanted Clothes", 15,
-               25, 25, 25,13),
-         Enemy("Expirienced Armed Bum", 35, "Handmade Sword", 45, 7, 25, "Coat", 15, 10, 30, 30,15),
-         Enemy("Goblin Wizard", 30, "Magic Stick 3000", 35, 40, 55, "Enchanted Set Of Clothes", 15, 20, 32, 30,15),
-         Enemy("Zombie", 40, "Hands? Jaw?", 45, 3, 30, "Nice Set Of Clothes", 20, 15, 35, 35,15),
-         Enemy("Bear", 50, "Paws", 40, 2, 40, "Nothing...", 10, 0, 40, 41,20),
-         Enemy("Armed Guy", 50, "Lock18", 40, 50, 30, "Light Gear", 20, 10, 50, 50,20),
-         Enemy("Armed Guy", 55, "Handmade SMG", 40, 50, 40, "Light Gear With A Plate Carrier", 30, 10, 50, 50,23),
-         Enemy("Armed Guy", 55, "Handmade Thompson", 55, 60, 50, "Roadsign Gear", 35, 10, 55, 55,25), ]
+               25, 25, 25, 13),
+         Enemy("Expirienced Armed Bum", 35, "Handmade Sword", 45, 7, 25, "Coat", 15, 10, 30, 30, 15),
+         Enemy("Goblin Wizard", 30, "Magic Stick 3000", 35, 40, 55, "Enchanted Set Of Clothes", 15, 20, 32, 30, 15),
+         Enemy("Zombie", 40, "Hands? Jaw?", 45, 3, 30, "Nice Set Of Clothes", 20, 15, 35, 35, 15),
+         Enemy("Bear", 50, "Paws", 40, 2, 40, "Nothing...", 10, 0, 40, 41, 20),
+         Enemy("Armed Guy", 50, "Lock18", 40, 50, 30, "Light Gear", 20, 10, 50, 50, 20),
+         Enemy("Armed Guy", 55, "Handmade SMG", 40, 50, 40, "Light Gear With A Plate Carrier", 30, 10, 50, 50, 23),
+         Enemy("Armed Guy", 55, "Handmade Thompson", 55, 60, 50, "Roadsign Gear", 35, 10, 55, 55, 25), ]
 
 armor = [Armor("Burlap Clothes", 10, 5),
          Armor("Old Cape", 5, 15),
@@ -597,7 +592,7 @@ case_types = {
         2: "Survival Kit",
         3: "Military First Aid Kit"
     },
-    'weapons': {
+    'weapon': {
         1: "Ящик с простым оружием",
         2: "Ящик с оружием среднего качества",
         3: "Ящик с элитным оружием"
@@ -611,7 +606,7 @@ case_types = {
 
 def Events(character, currentEnemy):
     events = ["Trader", "Fight", "CrateFound", "NothingHappened"]
-    eventchance = [13, 60, 7, 20]
+    eventchance = [100, 0, 300, 20]
     eventchoice = random.choices(events, weights=eventchance)[0]
     if eventchoice == "Trader":
         pass
@@ -619,9 +614,9 @@ def Events(character, currentEnemy):
         LutiyFight(character, currentEnemy)
         return True
     elif eventchoice == "CrateFound":
-        print("потом напишу")
-        # CaseOpen(character)
-        # return True
+        # print("потом напишу")
+        CaseOpen(character)
+        return True
         pass
     elif eventchoice == "NothingHappened":
         print("Вы шли довольно долго не найдя абсолютно ничего и решили передохнуть.")
@@ -638,13 +633,14 @@ def Main():
     print("Добро пожаловать в .")
     input("Нажмите ENTER чтобы начать.")
     gameName = input("Придумайте имя вашего персонажа ")
-    player1 = Character(gameName, 100, "Кулаки", 5, 5, 1, 1,0)
+    player1 = Character(gameName, 100, "Кулаки", 5, 5, 1, 1, 0)
     print(f"{player1.name} создан.")
     print(
         f"Имя персонажа: {player1.name}.\nЗдоровье персонажа: {player1.hp}.\nОружие персонажа: {player1.gun},\nСкорость персонажа: {player1.speed}.\nУрон персонажа: {player1.damage}.\n Дальность оружия персонажа: {player1.fRange}.\nТочность персонажа: {player1.gAccuracy}.\nОпыта до следующего уровня осталось: {player1.remainingXp}.")
     # Попробовать добавить меню действий и ивенты
     while player1.hp > 0:
-        print("Нажмите 1 для того чтобы осмотреться.\nНажмите 2 для открытия инвентаря.\nНажмите 3 для просмотра характеристик.\nНажмите 4 для распределения очков таланта.\nНажмите 0 для закрытия игры.")
+        print(
+            "Нажмите 1 для того чтобы осмотреться.\nНажмите 2 для открытия инвентаря.\nНажмите 3 для просмотра характеристик.\nНажмите 4 для распределения очков таланта.\nНажмите 0 для закрытия игры.")
         try:
             menuChoice = int(input())
         except (ValueError, TypeError, UnboundLocalError):
@@ -656,7 +652,7 @@ def Main():
         if menuChoice == 1:
             print("Осмотревшись вы обнаружили себя в густом, болотистом лесу. Вы решили пройти дальше.")
             currentEnemyM = Enemy.ChoiceEnemy(player1.cLvl)
-            Events(player1,currentEnemyM)
+            Events(player1, currentEnemyM)
         if menuChoice == 2:
             player1.inventoryView()
         if menuChoice == 3:
@@ -666,5 +662,7 @@ def Main():
             player1.talentUse()
         if menuChoice == 0:
             break
-#добавить больше кнопок для выбора, дописать инвентарь создав список в персонаже и напимать функцию которая этот список показывает при помощи цикла for и  обращения к конкретным атрибутам
+
+
+# добавить больше кнопок для выбора, дописать инвентарь создав список в персонаже и напимать функцию которая этот список показывает при помощи цикла for и  обращения к конкретным атрибутам
 Main()
